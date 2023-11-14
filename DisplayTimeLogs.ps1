de0e5413046ebf7d0a4497d6f9a1925aeb4cf518
Chris Nunes <chrisnunes57@gmail.com>
2023-11-14 13:37:14 -0800

    Update DisplayTimeLogs.ps1
    display average of all offsets at the end

--- DisplayTimeLogs.ps1
+++ DisplayTimeLogs.ps1
@@ -6,12 +6,13 @@ function Format-PhaseOffset($seconds) {
 }
 
 # get all time events where we log the current phaseOffset
-$syncEvents = Get-WinEvent "Microsoft-Windows-Time-Service/Operational" | Where { $_.Id -eq "260" }
+# 260 is the event code for status logs, and we filter out everything before 11/11/2023 (when we started processing)
+$syncEvents = Get-WinEvent "Microsoft-Windows-Time-Service/Operational" | Where { $_.Id -eq "260" } | where {$_.TimeCreated -gt (Get-Date -Date "11/11/2023 00:00:00 AM") }
 
 # use regex matching to parse all of the properties that we need 
 $formattedEvents = $syncEvents | Select-Object @{
     Name = "PhaseOffset"
-    Expression = { $_.Message -match "Phase Offset: (.*)s" | Out-Null; Format-PhaseOffset $matches[1] }
+    Expression = { $_.Message -match "Phase Offset: (.*)s" | Out-Null; $matches[1] }
 }, @{
     Name = "LastSync"
     Expression = { $_.Message -match "Last Successful Sync Time: (.*)" | Out-Null; $matches[1] }
@@ -20,4 +21,9 @@ $formattedEvents = $syncEvents | Select-Object @{
     Expression = { $_.Message -match "Source: (.*)\s" | Out-Null; $matches[1] }
 },  TimeCreated
 
+# print all events
 $formattedEvents | Format-List TimeCreated, PhaseOffset, LastSync, TimeSource
+
+#calculate average
+$averageOffset = $formattedEvents | Measure-Object -Property PhaseOffset -Average | Select-Object -Property Average, Count
+echo "Average offset over $($averageOffset.Count) events is $(Format-PhaseOffset $averageOffset.Average)"
